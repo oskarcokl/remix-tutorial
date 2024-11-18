6d413963b9c12ba3ecab37b76e18d72d4ed2b558
coskar <oskar.cokl@iprom.si>
2024-11-18 14:41:50 +0100

    feat: Fetching contacts from the server.

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import type { LinksFunction } from "@remix-run/node";
 import appStylesHref from "./app.css?url";
-
+import { json } from "@remix-run/node";
 import {
   Form,
   Links,
@@ -9,11 +9,21 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+
+import { getContacts } from "./data";
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: appStylesHref }];
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -41,14 +51,30 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
         <div id="detail">
